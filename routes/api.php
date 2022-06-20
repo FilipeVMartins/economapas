33087a7c084b4aee3c855da51932cceee3950465
FilipeVMartins <filipe_2021@hotmail.com>
2022-06-20 11:30:29 -0300

    readme

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function(){
+Route::middleware('auth:sanctum')->group(function() {
 
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 
